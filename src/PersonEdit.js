bcebebe88f25664d8e39763a617909396360291a
Brent <bgardner@squarelabs.net>
2017-11-09 23:11:17 +0100

    Can type into edit box

--- src/PersonEdit.js
+++ src/PersonEdit.js
@@ -2,19 +2,41 @@ import React, { Component } from 'react';
 
 class PersonEdit extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = JSON.parse(JSON.stringify(props.person))
+    }
+
+    changed = (event) => {
+        console.log('test')
+        const newState = {};
+        newState[event.target.name] = event.target.value;
+        this.setState(Object.assign({}, this.state, newState));
+    }
+
     render() {
         return (
             <div>
                 <div>
                     <span>First Name</span>
-                    <input type="text" value={this.props.person.firstName} />
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.changed}
+                    />
                 </div>
                 <div>
                     <span>Last Name</span>
-                    <input type="text" value={this.props.person.lastName} />
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.changed}
+                    />
                 </div>
-                <input type="button" onClick={this.props.update} value="Save"/>
-                <input type="button" onClick={this.props.cancel} value="Cancel"/>
+                <input type="button" onClick={this.props.update} value="Save" />
+                <input type="button" onClick={this.props.cancel} value="Cancel" />
             </div>
         );
     }
