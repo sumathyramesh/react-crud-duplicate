7f61d95906f0ef7fd24bfcf2c64554e4ee0975cd
Brent <bgardner@squarelabs.net>
2017-11-09 22:35:47 +0100

    PersonList

--- src/App.js
+++ src/App.js
@@ -7,15 +7,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: 'list',
+      selectedPerson: undefined,
       people: [
-        {firstName: 'Brent', lastName: 'Gardner'},
-        {firstName: 'Rachel', lastName: 'Koldenhoven'}
+        { firstName: 'Brent', lastName: 'Gardner' },
+        { firstName: 'Rachel', lastName: 'Koldenhoven' }
       ]
     }
   }
 
+  personSelected(person) {
+    const newState = JSON.parse(JSON.stringify(this.state));
+    newState.selectedPerson = person;
+    newState.view = 'edit';
+    this.setState(newState);
+  }
+
+  get view() {
+    switch (this.state.view) {
+      case 'list':
+        return <PersonList
+          people={this.state.people}
+          personSelected={p => this.personSelected(p)}
+        />
+      default:
+        throw new Error('Unknown view: ', this.state.view)
+    }
+  }
+
   render() {
-    return <PersonList people={this.state.people} />;
+    return this.view;
   }
 }
 
--- src/PersonList.js
+++ src/PersonList.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 
 class PersonList extends Component {
 
-  render() {
-    return (
-        <ul>
-            {this.props.people.map((person) => <li>{person.firstName} {person.lastName}</li>)}
-        </ul>
-    );
-  }
+    get people() {
+        return this.props.people.map((person, idx) => {
+            return (<a key={idx} href="#" onClick={() => this.props.personSelected(person)}>
+                <li>{person.firstName} {person.lastName}</li>
+            </a>)
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.people}
+            </ul>
+        );
+    }
 }
 
 export default PersonList;
