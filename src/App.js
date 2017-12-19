91dd8a459c515253bd21cb259f0448318bb93f62
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-12-19 08:27:55 +0100

    Fix rebase conflicts

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
         super(props);
         this.state = {
           people: [
-            {firstName: 'Margaret', lastName: 'Hamilton'},
-            {firstName: 'Donald', lastName: 'Knuth'}
+              {firstName: 'Margaret', lastName: 'Hamilton', id: '81d6ff6c-10f4-4db0-88f2-1ebf789b7779'},
+              {firstName: 'Donald', lastName: 'Knuth', id: 'f515b8de-5916-47db-9fa8-75efe4a5ebb2'}
           ],
           view: 'PersonList',
           selectedPerson: undefined
