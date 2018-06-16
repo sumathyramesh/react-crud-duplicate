d901f24af814a1931f6d0f70ac383e832aea2fd4
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-06-16 11:54:33 +0530

    code formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PersonList from './PersonList';
 import './styles/App.css';
 
@@ -8,23 +8,23 @@ const uuid = require('uuid4');
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      people: [
-        {firstName: 'Margaret', lastName: 'Hamilton', id: '81d6ff6c-10f4-4db0-88f2-1ebf789b7779'},
-        {firstName: 'Donald', lastName: 'Knuth', id: 'f515b8de-5916-47db-9fa8-75efe4a5ebb2'}
-      ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            people: [
+                {firstName: 'Margaret', lastName: 'Hamilton', id: '81d6ff6c-10f4-4db0-88f2-1ebf789b7779'},
+                {firstName: 'Donald', lastName: 'Knuth', id: 'f515b8de-5916-47db-9fa8-75efe4a5ebb2'}
+            ]
+        }
     }
-  }
 
-  render() {
-    return (
-        <div className="Main">
-            <PersonList people={this.state.people} />
-        </div>
-    ); 
-  }
+    render() {
+        return (
+            <div className="Main">
+                <PersonList people={this.state.people}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/PersonList.js
+++ src/PersonList.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class PersonList extends Component {
 
-  render() {
-    return (
-        <ul className="PersonList">
-            {this.props.people.map((person) => <li key={person.id}>{person.firstName} {person.lastName}</li>)}
-        </ul>
-    );
-  }
+    render() {
+        return (
+            <ul className="PersonList">
+                {this.props.people.map((person) => <li key={person.id}>{person.firstName} {person.lastName}</li>)}
+            </ul>
+        );
+    }
 }
 
 export default PersonList;
